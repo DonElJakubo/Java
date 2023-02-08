2023887b684fd873713039b8da42054e74e06163
DonElJakubo <92189465+DonElJakubo@users.noreply.github.com>
2023-02-08 23:15:53 +0100

    Delete src/brudnopis/NokiaJavaFullstackWorkingStudent directory

--- src/brudnopis/NokiaJavaFullstackWorkingStudent/Java/DefinicjePlusPrzyklady/KlasyIInterfejsy/Przyklad/ExampleInterface.java
+++
@@ -1,8 +0,0 @@
-package rekru.rozmowy.NokiaJavaFullstackWorkingStudent.Java.DefinicjePlusPrzyklady.KlasyIInterfejsy.Przyklad;
-
-//Interfejs:
-
-public interface ExampleInterface {
-    public String getName();
-    public int getAge();
-}
--- src/brudnopis/NokiaJavaFullstackWorkingStudent/Java/DefinicjePlusPrzyklady/SkladoweProgramu/Przyklad/MyInterface.java
+++
@@ -1,6 +0,0 @@
-package rekru.rozmowy.NokiaJavaFullstackWorkingStudent.Java.DefinicjePlusPrzyklady.SkladoweProgramu.Przyklad;
-
-// Definicja interfejsu
-public interface MyInterface {
-    public void doSomething();
-}
